5487c71d54df649d53015108ae255201e9e1306a
Julyme3 <jg0311@bk.ru>
2022-11-28 17:11:37 +0300

    webpack: fix reload html

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,82 @@
 const path = require('path');
 const HTMLWebpackPlugin = require('html-webpack-plugin');
-const {CleanWebpackPlugin} = require('clean-webpack-plugin');
-const CopyPlugin = require("copy-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 
 const isDev = process.env.NODE_ENV === 'development';
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: isDev ? '[name].js' : '[name].[hash].js',
-        path: path.resolve(__dirname, 'dist')
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: isDev ? '[name].js' : '[name].[hash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
-    resolve: {
-        extensions: ['.js', '.ts'],
-        alias: {
-            '@': path.resolve(__dirname, 'src')
-        },
+  },
+  devtool: isDev ? 'source-map' : 'eval',
+  devServer: {
+    static: './dist',
+    port: 3000,
+    compress: true,
+    hot: false,
+    historyApiFallback: true,
+    open: true,
+    liveReload: true,
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new CleanWebpackPlugin(),
+    // new CopyPlugin({
+    //     patterns: [
+    //         {
+    //             from: path.resolve(__dirname, 'src/assets'),
+    //             to: path.resolve(__dirname, 'dist/assets')
+    //         }
+    //     ]
+    // }),
+    new MiniCssExtractPlugin({
+      filename: isDev ? '[name].css' : '[name].[hash].css',
+    }),
+    new ESLintPlugin({
+      fix: false,
+      failOnWarning: false,
+    }),
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
     },
-    devtool: isDev ? 'source-map' : 'eval',
-    devServer: {
-        port: 3000,
-        compress: true,
-        historyApiFallback: true,
-        open: true,
-        hot: isDev,
-    },
-    plugins: [
-        new HTMLWebpackPlugin({
-            template: './src/index.html'
-        }),
-        new CleanWebpackPlugin(),
-        // new CopyPlugin({
-        //     patterns: [
-        //         {
-        //             from: path.resolve(__dirname, 'src/assets'),
-        //             to: path.resolve(__dirname, 'dist/assets')
-        //         }
-        //     ]
-        // }),
-        new MiniCssExtractPlugin({
-            filename: isDev ? '[name].css' : '[name].[hash].css'
-        }),
-      new ESLintPlugin()
+    minimizer: [new CssMinimizerPlugin(), '...'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.less$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader'],
+      },
+      {
+        test: /\.(png|jpe?g|svg|gif)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.(ttf|woff|woff2|eot)$/,
+        type: 'asset/resource',
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
     ],
-    optimization: {
-        splitChunks: {
-            chunks: "all"
-        },
-        minimizer: [new CssMinimizerPlugin(), "..."],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.less$/i,
-                use: [MiniCssExtractPlugin.loader, 'css-loader', 'less-loader']
-            },
-            {
-                test: /\.(png|jpe?g|svg|gif)$/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.(ttf|woff|woff2|eot)$/,
-                type: 'asset/resource'
-            },
-            {
-                test: /\.m?js$/,
-                exclude: /node_modules/,
-                use: ['babel-loader']
-            }
-        ]
-    }
-}
+  },
+};
